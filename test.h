754f97a925f12db122aa4c8f369655b02f9d61eb
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 20:36:36 +0530

    Update test.h

--- test.h
+++ test.h
@@ -1,4 +1,6 @@
 void test()
 {
-  
+  printf("Check if print on console is successful);
+  printSensorDataToConsole(temperature,chargerate);
+  assert(verifyPrintToConsole(readings_count_check)==1);           
 }
