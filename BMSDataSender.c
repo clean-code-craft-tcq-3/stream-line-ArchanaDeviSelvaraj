da4157021eb5b5585cd1f2b92d22823d1ee2348c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 20:15:30 +0530

    Update BMSDataSender.c

--- BMSDataSender.c
+++ BMSDataSender.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 
-void readSensorDataFromFile
+void readSensorDataFromFile (float* temperature, float* chargerate)
 { 
     FILE *fp; 
-    float temperature,chargerate;
+    float temp_data,chargerate_data;
     fp =fopen("SensorData.txt","r");
     if (!fp)
+    {
     printf("\n File doesn't exist");
-    while (fgets(temperature,chargerate,1000, fp)!=NULL)
-    printf("%f","%f",temperature,chargerate);
+    }
+    else
+    {
+    for(int index=0; fscanf(fp, "%f %f\n",&temp_data,&chargerate_data)!=EOF; index++)
+    {
+      *(temperature+i) = temp_data;
+      *(chargerate+i) = chargerate_data;
+    }
     fclose(fp); 
  }
