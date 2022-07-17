b85ec99e193f2e194d839c7f08176b6d93c3808e
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 21:34:13 +0530

    Update test.h

--- test.h
+++ test.h
@@ -5,7 +5,7 @@ void test()
 {
   float temperature,chargerate;
   int readings_count_check;
-  printf("Check if print on console is successful);
+  printf("Check if print on console is successful");
   printSensorDataToConsole(temperature,chargerate);
   assert(verifyPrintToConsole(readings_count_check)==1);           
 }
