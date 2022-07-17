0dfa982572abd3b792ca38e77a4f9eefb58dd590
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 20:37:48 +0530

    Update BMSDataSender.h

--- BMSDataSender.h
+++ BMSDataSender.h
@@ -1,1 +1,5 @@
 #define total_readings_count 50
+
+void readSensorDataFromFile(float* temperature, float* chargerate);
+int printSensorDataToConsole(float* temperature, float* chargerate);
+int verifyPrintToConsole(int readings_count_check);
