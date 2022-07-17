832c6242ce8b5b30c16beb7e4b11cfbda840ad55
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 21:31:18 +0530

    Update test.h

--- test.h
+++ test.h
@@ -1,9 +1,10 @@
 #include <assert.h>
 #include "BMSDataSender.h"
-#include "BMSDataSender.c"
 
 void test()
-{
+{.
+  float temperature,chargerate;
+  int readings_count_check;
   printf("Check if print on console is successful);
   printSensorDataToConsole(temperature,chargerate);
   assert(verifyPrintToConsole(readings_count_check)==1);           
