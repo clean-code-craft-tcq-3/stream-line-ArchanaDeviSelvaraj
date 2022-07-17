a56bfc2d9670c1d0b16713d66ec245f2d5903b97
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-17 20:02:47 +0530

    Update BMSDataSender.c

--- BMSDataSender.c
+++ BMSDataSender.c
@@ -7,7 +7,7 @@ void readSensorDataFromFile
     fp =fopen("SensorData.txt","r");
     if (!fp)
     return 1;
-    while (fgets(temperature,chargerate,1000, fp)!=NULL)// reading file content
+    while (fgets(temperature,chargerate,1000, fp)!=NULL)
     printf("%f","%f",temperature,chargerate);
     fclose(fp); 
     return 0;
