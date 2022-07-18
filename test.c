fa6f8e83c3950b687683df12aa17821cbb109e3b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 15:10:07 +0530

    Update test.c

--- test.c
+++ test.c
@@ -13,8 +13,8 @@ int main()
      float chargerate[total_readings_count] ={0};
      readSensorDataFromFile(temperature,chargerate);
      printSensorDataToConsole(temperature, chargerate);
-     printf("\n value %f,*(temperature+0));
-      printf("\n value %f,*(chargerate+0));       
+     printf("\n value %f,*(temperature+0)");
+     printf("\n value %f,*(chargerate+0)");       
      //assert(*(temperature+0)== 10);
      //assert(*(chargerate+0)== 0.100000);
      return 0;
