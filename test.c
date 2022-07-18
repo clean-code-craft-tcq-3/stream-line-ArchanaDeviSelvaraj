36c46ad3b18340cfe084aea82fc82a68d9592bfe
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 15:52:06 +0530

    Update test.c

--- test.c
+++ test.c
@@ -19,6 +19,7 @@ int main()
      printf("\n Temperature : %f \t Charge rate : %f \n",*(temperature_array+24),*(chargerate_array+24));
      printf("\n Temperature : %f \t Charge rate : %f \n",*(temperature_array+49),*(chargerate_array+49)); 
      assert(*(temperature_array+24) == 2);
+     assert(*(chargerate_array+24) == 0.100000);
      return 0;
  }
 
