6f034edea3facda6d4ef870a55dd043d193b8bba
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 15:13:17 +0530

    Update test.c

--- test.c
+++ test.c
@@ -9,12 +9,12 @@ int main()
      assert(verifyPrintToConsole(50)==1);
      assert(verifyPrintToConsole(0)==0);
      printf("\nTo Verify read and print fucntions");
-     float temperature[total_readings_count] ={0};
-     float chargerate[total_readings_count] ={0};
-     readSensorDataFromFile(temperature,chargerate);
-     printSensorDataToConsole(temperature, chargerate);
-     printf("\n value %f,*(temperature+0)");
-     printf("\n value %f,*(chargerate+0)");       
+     float temperature_array[total_readings_count] ={0};
+     float chargerate_array[total_readings_count] ={0};
+     readSensorDataFromFile(temperature_array,chargerate_array);
+     printSensorDataToConsole(temperature_array,chargerate_array);
+     printf("\n value %f,*(temperature_array+0)");
+     printf("\n value %f,*(chargerate_array+0)");       
      //assert(*(temperature+0)== 10);
      //assert(*(chargerate+0)== 0.100000);
      return 0;
