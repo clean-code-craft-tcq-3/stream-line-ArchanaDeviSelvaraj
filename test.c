f56afe7a2a1723ba619e630338a6ec417eec6f8b
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 15:40:13 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,18 +5,22 @@ int main()
  {
      printf("\nCheck if print on console is successful");
      assert(communicateSensorData()==1);    
+ 
      printf("\nTo check verifyPrintToConsole function"); 
      assert(verifyPrintToConsole(50)==1);
      assert(verifyPrintToConsole(0)==0);
+ 
      printf("\nTo Verify read and print fucntions");
      float temperature_array[total_readings_count] ={0};
      float chargerate_array[total_readings_count] ={0};
      readSensorDataFromFile(temperature_array,chargerate_array);
+ 
      printf("\n Temperature : %f \t Charge rate : %f \n",*(temperature_array+0),*(chargerate_array+0)); 
      float expected_temp_value_1 = *(temperature_array+0);
      float expected_chargerate_value_1 = *(chargerate_array+0);
      assert(expected_temp_value_1 == 10);
      assert(expected_chargerate_value_1 == 0.10);
+ 
      printf("\n Temperature : %f \t Charge rate : %f \n",*(temperature_array+49),*(chargerate_array+49)); 
      float expected_temp_value_50 = *(temperature_array+49);
      float expected_chargerate_value_50 = *(chargerate_array+49);
