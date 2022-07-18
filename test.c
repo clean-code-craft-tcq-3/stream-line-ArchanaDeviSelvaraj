383574d957264fee78397bb4e80dd6f3eb05e80a
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 15:55:15 +0530

    Update test.c

--- test.c
+++ test.c
@@ -20,7 +20,7 @@ int main()
      printf("\n Temperature : %f \t Charge rate : %f \n",*(temperature_array+49),*(chargerate_array+49)); 
      assert(*(temperature_array+24) == 2);
      float chargerate_25 = *(chargerate_array+24);
-     assert(chargerate_25 == 0.100000);
+     assert(chargerate_25 == 0.10);
      return 0;
  }
 
