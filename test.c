341d2880d83d8a7e873bbff3d0176bb8512b023c
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 14:37:54 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,9 +5,9 @@ int main()
  {
      printf("\nCheck if print on console is successful");
      assert(communicateSensorData()==1);    
-     return 0;
      printf("\nTo check verifyPrintToConsole function"); 
      assert(verifyPrintToConsole(50)==1);
      assert(verifyPrintToConsole(0)==0);
+     return 0;
  }
 
