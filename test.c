13ca0e38f6128125618ae6e9ae08772f6457ed82
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-07-18 16:19:16 +0530

    Update test.c

--- test.c
+++ test.c
@@ -13,9 +13,16 @@ int main()
      printf("\nTo Verify read and print fucntions");
      float temperature_array[total_readings_count] ={0};
      float chargerate_array[total_readings_count] ={0};
+     float output_data[5][2] = {{10,0.10}, {26,0.15}, {22,0.20},{36,0.78},{43,0.10}};
      readSensorDataFromFile(temperature_array,chargerate_array);
  
-     printf("\n Temperature : %f \t Charge rate : %f \n",temperature_array[0],chargerate_array[0]); 
+     int count= 0, index= 0;
+     for(index=0;index<4;index++)
+     {
+       assert(temperature_array[count] == output_data[index][0]);
+       assert(chargerate_array[count] == output_data[index][1]);
+       count++;
+     }
      
      return 0;
  }
