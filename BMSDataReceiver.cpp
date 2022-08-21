064a160de1fc9af0bf8d828d2681e78c01560d47
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-21 15:32:55 +0530

    Update BMSDataReceiver.cpp

--- BMSDataReceiver.cpp
+++ BMSDataReceiver.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 void readSensorDataFromConsole(float* temperature, float* chargeRate)
 {
-    /*char line[50];
+    char line[50];
     cin.getline(line,50);
-    for(int index = 0; index < total_readings_count+1; index++)
+    for(int index = 1; index < total_readings_count+1; index++)
     {
         char line[100],a[20],b[20],c[20],d[20];
         float num1, num2;
@@ -18,27 +18,7 @@ void readSensorDataFromConsole(float* temperature, float* chargeRate)
         //scanf("%f\t %f",&temperature[index],&chargeRate[index]);
         
         scanf("%s\t %s\t %f\t %s\t %s\t %f",&a,&b,&num1,&c,&d,&num2);
-        printf("%s\t %s\t %f\t %s\t %s\t %f\n",a,b,num1,c,d,num2);*/
-    string data;
-    FILE* consoleOutput;
-    const int max_size = 1000;
-    char InputData[max_size];
-    
-    consoleOutput = popen(cmd.c_str(),"r");
-    if(consoleOutput)
-    {
-        while(!feof(consoleOutput))
-        {
-            if(fgets(InputData,max_size, consoleOutput) != NULL)
-            {
-                data.append(InputData);
-            }
-        }
-        pclose(stream);
-    }
-    
-    cout<<data;
-    
+        printf("%s\t %s\t %f\t %s\t %s\t %f\n",a,b,num1,c,d,num2);
     }
 }
 
