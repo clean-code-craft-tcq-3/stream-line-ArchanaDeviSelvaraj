10f50632c88d2d00356c27fd36adcd3ab7e875e1
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-21 14:55:07 +0530

    Update BMSDataReceiver.cpp

--- BMSDataReceiver.cpp
+++ BMSDataReceiver.cpp
@@ -9,13 +9,14 @@ void readSensorDataFromConsole(float* temperature, float* chargeRate)
     for(int index = 1; index < total_readings_count+1; index++)
     {
         char line[100],a[20],b[20],c[20],d[20];
+        float num1, num2;
         //scanf("%s",line[index]);
         //cin.getline(line,50);
         //printf("%s\n",line);
         //scanf("%f\t %f",&temperature[index],&chargeRate[index]);
         
-        scanf("%s\t %s\t %f\t %s\t %s\t %f",&a,&b,&temperature[index],&c,&d,&chargeRate[index]);
-        printf("%f\t %f",temperature[index],chargeRate[index]);
+        scanf("%s\t %s\t %f\t %s\t %s\t %f",&a,&b,&num1,&c,&d,&num2);
+        printf("%s\t %s\t %f\t %s\t %s\t %f\n",a,b,num1,c,d,num2);
     }
 }
 
